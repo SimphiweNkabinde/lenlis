0f9a50583e6bfe9f05b7bca00041962cf4300172
Simphiwe Nkabinde <simphiwenkabinde.dev@gmail.com>
2026-06-30 15:33:37 +0200

    add pinned tab to my lists

--- components/list-tabs.tsx
+++ components/list-tabs.tsx
@@ -8,9 +8,10 @@ import ListCollectionContainer from "./list-collection-container"
 
 export function ListTabs() {
     return (
-        <Tabs defaultValue=" all" className="p-4 h-full">
-            <TabsList className="bg-transparent justify-between w-full px-0">
+        <Tabs defaultValue="all" className="p-4 h-full">
+            <TabsList className="bg-transparent justify-between w-full px-0 overflow-x-scroll scrollbar-none">
                 <TabsTrigger className="flex-0 text-current/75 data-active:bg-muted rounded-full p-4" value="all">All</TabsTrigger>
+                <TabsTrigger className="flex-0 text-current/75 data-active:bg-muted rounded-full p-4" value="pinned">Pinned</TabsTrigger>
                 <TabsTrigger className="flex-0 text-current/75 data-active:bg-muted rounded-full p-4" value="owned-by-you">Owned by you</TabsTrigger>
                 <TabsTrigger className="flex-0 text-current/75 data-active:bg-muted rounded-full p-4" value="shared-with-you">Shared with you</TabsTrigger>
             </TabsList>
@@ -18,6 +19,11 @@ export function ListTabs() {
                 <ListCollectionContainer
                     emptyTitle="No lists yet"
                     emptySubtitle="All your lists will appear here" />
+            </TabsContent>
+            <TabsContent value="pinned">
+                <ListCollectionContainer
+                    emptyTitle="No pinned lists yet"
+                    emptySubtitle="All your pinned lists will appear here" />
             </TabsContent>
             <TabsContent value="owned-by-you">
                 <ListCollectionContainer
