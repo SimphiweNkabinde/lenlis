bcba7bf77be02378626e8f6da27fce4192de0dda
Simphiwe Nkabinde <simphiwenkabinde.dev@gmail.com>
2026-07-03 11:50:19 +0200

    Show lists on "My Lists" page

---
+++ app/lists/_components/list-collection-container.tsx
@@ -0,0 +1,47 @@
+import { DotIcon, ListIcon } from "lucide-react"
+import moment from "moment"
+import Link from "next/link"
+
+type ListType = {
+    name: string
+}
+
+export default function ListCollectionContainer({ listCollection = [], emptyTitle = "", emptySubtitle = "" }: { listCollection?: { id: string, name: string, createdAt: string, members: number }[], emptyTitle?: string, emptySubtitle?: string }) {
+
+    if (!listCollection || !listCollection.length) return <EmptyCollection emptySubtitle={emptySubtitle} emptyTitle={emptyTitle} />
+
+    return (
+        <ul className="flex flex-col gap-4">
+            {listCollection.map(list => (
+                <li className="px-4 py-1 hover:bg-muted/50">
+                    <Link href={`/lists/${list.id}/edit`} className="flex items-center gap-3">
+                        <div className="bg-muted rounded-lg size-10 flex justify-center items-center">
+                            <ListIcon className="size-4" />
+                        </div>
+                        <div className="flex flex-col">
+                            <div className="text-base"> {list.name}</div>
+                            <div className="flex items-center text-current/60 text-xs">
+                                <span>{moment(list.createdAt).format("MMM D")}</span>
+                                <DotIcon />
+                                <span>{list.members} collaborator{list.members > 1 ? "s" : ""}</span>
+                            </div>
+                        </div>
+                    </Link>
+                </li>
+            ))}
+        </ul>
+    )
+}
+
+
+function EmptyCollection({ emptySubtitle, emptyTitle }: { emptyTitle: string, emptySubtitle: string }) {
+    return (
+        <div className="h-full flex flex-col items-center justify-center">
+            <div className="flex flex-col items-center gap-2">
+                <ListIcon className="bg-muted rounded-xl p-2 size-8" />
+                <div className="font-semibold text-xl">{emptyTitle}</div>
+                <div className="text-current/50 text-lg text-center">{emptySubtitle}</div>
+            </div>
+        </div>
+    )
+}
--- app/lists/page.tsx
+++ app/lists/page.tsx
@@ -1,11 +1,20 @@
 import ListPageHeader from "@/components/list-page-header";
-import { ListTabs } from "@/components/list-tabs";
+import { ListTabs } from "@/app/lists/_components/list-tabs";
+import { createClient } from "@/lib/supabase/server";
 
-export default function Page() {
+export default async function Page() {
+
+    const supabase = await createClient()
+    const { data: userData } = await supabase.auth.getUser()
+    const { data, error } = await supabase.from("lists")
+        .select("id, name, list_members!inner (user_id, role), createdAt:created_at")
+        .eq("list_members.user_id", userData?.user?.id)
+
     return (
         <div className="flex h-dvh relative flex flex-col overflow-hidden">
             <ListPageHeader />
-            <ListTabs />
+            <ListTabs
+                lists={data?.map(({ id, name, list_members, createdAt }) => ({ id, name, role: list_members.find(i => i.user_id == userData?.user?.id)?.role, members: list_members.length, createdAt }))!} />
         </div>
     )
 }
