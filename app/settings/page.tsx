a19ed6fd3358ee58dc8563433c5d68f69427b54f
Simphiwe Nkabinde <simphiwenkabinde.dev@gmail.com>
2026-07-19 14:03:12 +0200

    display user profile name & username. update setting items

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,13 +1,19 @@
 import { Avatar, AvatarBadge, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-import { Button, buttonVariants } from "@/components/ui/button"
-import { Item, ItemActions, ItemContent, ItemMedia, ItemTitle } from "@/components/ui/item"
-import { ArrowLeftIcon, BellIcon, ChevronRightIcon, CircleDollarSignIcon, CircleQuestionMarkIcon, FileTextIcon, LogOutIcon, PencilIcon, ShapesIcon, ShieldAlertIcon } from "lucide-react"
+import { buttonVariants } from "@/components/ui/button"
+import { Item, ItemActions, ItemContent, ItemDescription, ItemMedia, ItemTitle } from "@/components/ui/item"
+import { ArrowLeftIcon, BellIcon, ChevronRightIcon, CircleDollarSignIcon, CircleQuestionMarkIcon, FileTextIcon, GemIcon, LogOutIcon, PencilIcon, ShapesIcon, ShieldAlertIcon } from "lucide-react"
 import Link from "next/link"
 import LogoutItem from "./_components/logout-item"
 import EmailItem from "./_components/email-item"
 import clsx from "clsx"
+import { createClient } from "@/lib/supabase/server"
+import { redirect } from "next/navigation"
 
-export default function Page() {
+export default async function Page() {
+    const supabase = await createClient()
+    const { data: userData } = await supabase.auth.getUser()
+    if (!userData?.user) redirect("/")
+    const { data: profile } = await supabase.from("profiles").select("name, username, avatar_url").eq("id", userData.user?.id).single()
     return (
         <div className="h-dvh relative flex flex-col overflow-scroll p-4">
             <div className="justify-between w-full flex items-center mb-5">
@@ -23,18 +29,33 @@ export default function Page() {
                         <PencilIcon />
                     </AvatarBadge>
                 </Avatar>
-                <div className="font-semibold text-xl">Jacob Mazibuko</div>
+                <div className="font-semibold text-xl">{profile?.name}</div>
+                <div className="text-base text-muted-foreground">@{profile?.username}</div>
             </div>
 
             <div className="flex w-full flex-col gap-3">
                 <EmailItem />
                 <Item variant="muted" size="default" className="" render={
                     <Link href="/settings/notifications">
-                        <ItemMedia>
+                        <ItemMedia className="h-full">
+                            <GemIcon className="size-5" />
+                        </ItemMedia>
+                        <ItemContent>
+                            <ItemTitle>Subscriptions</ItemTitle>
+                            <ItemDescription>Explore premium benefits</ItemDescription>
+                        </ItemContent>
+                        <ItemActions>
+                            <ChevronRightIcon className="size-4" />
+                        </ItemActions>
+                    </Link>} />
+                <Item variant="muted" size="default" className="" render={
+                    <Link href="/settings/notifications">
+                        <ItemMedia className="h-full">
                             <BellIcon className="size-5" />
                         </ItemMedia>
                         <ItemContent>
                             <ItemTitle>Notifications</ItemTitle>
+                            <ItemDescription>Email and push notifications</ItemDescription>
                         </ItemContent>
                         <ItemActions>
                             <ChevronRightIcon className="size-4" />
@@ -42,59 +63,12 @@ export default function Page() {
                     </Link>} />
                 <Item variant="muted" size="default" className="" render={
                     <Link href="/settings/notifications">
-                        <ItemMedia>
-                            <CircleDollarSignIcon className="size-5" />
-                        </ItemMedia>
-                        <ItemContent>
-                            <ItemTitle>Upgrade to Premium</ItemTitle>
-                        </ItemContent>
-                        <ItemActions>
-                            <ChevronRightIcon className="size-4" />
-                        </ItemActions>
-                    </Link>} />
-                <Item variant="muted" size="default" className="" render={
-                    <Link href="/settings/notifications">
-                        <ItemMedia>
-                            <ShieldAlertIcon className="size-5" />
-                        </ItemMedia>
-                        <ItemContent>
-                            <ItemTitle>Privacy</ItemTitle>
-                        </ItemContent>
-                        <ItemActions>
-                            <ChevronRightIcon className="size-4" />
-                        </ItemActions>
-                    </Link>} />
-                <Item variant="muted" size="default" className="" render={
-                    <Link href="/settings/notifications">
-                        <ItemMedia>
-                            <ShapesIcon className="size-5" />
-                        </ItemMedia>
-                        <ItemContent>
-                            <ItemTitle>Terms of Service</ItemTitle>
-                        </ItemContent>
-                        <ItemActions>
-                            <ChevronRightIcon className="size-4" />
-                        </ItemActions>
-                    </Link>} />
-                <Item variant="muted" size="default" className="" render={
-                    <Link href="/settings/notifications">
-                        <ItemMedia>
+                        <ItemMedia className="h-full">
                             <CircleQuestionMarkIcon className="size-5" />
                         </ItemMedia>
                         <ItemContent>
-                            <ItemTitle>Suppport</ItemTitle>
-                        </ItemContent>
-                        <ItemActions>
-                            <ChevronRightIcon className="size-4" />
-                        </ItemActions>
-                    </Link>} />
-                <Item variant="muted" size="default" className="" render={
-                    <Link href="/settings/notifications">
-                        <ItemMedia>
-                            <FileTextIcon className="size-5" />
-                        </ItemMedia>
-                        <ItemContent>
-                            <ItemTitle>Community Guideline</ItemTitle>
+                            <ItemTitle>Help and feedback</ItemTitle>
+                            <ItemDescription>Contact us, issues, Terms & privacy</ItemDescription>
                         </ItemContent>
                         <ItemActions>
                             <ChevronRightIcon className="size-4" />
