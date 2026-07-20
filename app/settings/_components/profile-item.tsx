9db6ab2052b8e72134f57f35227ab95f7e00d187
Simphiwe Nkabinde <simphiwenkabinde.dev@gmail.com>
2026-07-20 18:59:56 +0200

    loading state UI for profile item

--- app/settings/_components/profile-item.tsx
+++ app/settings/_components/profile-item.tsx
@@ -2,23 +2,25 @@
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Item, ItemActions, ItemContent, ItemDescription, ItemMedia, ItemTitle } from '@/components/ui/item'
 import { useAuth } from '@/context/auth-provider'
+import clsx from 'clsx'
 import { ChevronRightIcon } from 'lucide-react'
 import Link from 'next/link'
 
 export default function ProfileItem() {
-    const { user } = useAuth()
+    const { user, loading } = useAuth()
     return (
+
         <Item variant="muted" size="default" className="mb-5" render={
             <Link href="/settings/profile">
                 <ItemMedia className="h-full">
-                    <Avatar className="size-10">
+                    <Avatar className={clsx("size-10", { "animate-pulse": loading })}>
                         <AvatarImage src={user?.avatarUrl} alt="@shadcn" />
                         <AvatarFallback className="text-xl">{user?.username?.charAt(0)}</AvatarFallback>
                     </Avatar>
                 </ItemMedia>
                 <ItemContent>
-                    <ItemTitle>{user?.name}</ItemTitle>
-                    <ItemDescription>@{user?.username}</ItemDescription>
+                    <ItemTitle className={clsx({ "w-25 h-4 bg-muted rounded mb-1": loading })}>{user?.name}</ItemTitle>
+                    <ItemDescription className={clsx({ "w-40 h-4 bg-muted rounded": loading })}>{user?.username && `@${user?.username}`}</ItemDescription>
                 </ItemContent>
                 <ItemActions>
                     <ChevronRightIcon className="size-4" />
